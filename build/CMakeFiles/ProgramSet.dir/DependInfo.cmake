76a279912bdd8c56288ab9dbd3f1fb533a695d0f
Sherzod <sherzik99@gmail.com>
2021-08-18 00:34:02 +0500

    Added insert function and prepare to test

---
+++ build/CMakeFiles/ProgramSet.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/projects/Set_Container/Set.cpp" "D:/projects/Set_Container/build/CMakeFiles/ProgramSet.dir/Set.cpp.obj"
+  "D:/projects/Set_Container/Test.cpp" "D:/projects/Set_Container/build/CMakeFiles/ProgramSet.dir/Test.cpp.obj"
+  "D:/projects/Set_Container/main.cpp" "D:/projects/Set_Container/build/CMakeFiles/ProgramSet.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ProgramSet.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProgramSet.dir/Set.cpp.obj"
+  "CMakeFiles/ProgramSet.dir/Test.cpp.obj"
+  "CMakeFiles/ProgramSet.dir/main.cpp.obj"
+  "ProgramSet.exe"
+  "ProgramSet.exe.manifest"
+  "ProgramSet.pdb"
+  "libProgramSet.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProgramSet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
