b5413775f964f2d92ccea83207b6d2e6d48efa2a
Sherzod <sherzik99@gmail.com>
2021-08-18 00:35:15 +0500

    modified set and cmakelists

--- Set.cpp
+++ Set.cpp
@@ -1,72 +1,99 @@
 #include "Set.h"
+#include <cstring>
 
-Set::Set(MemoryManager &mem) : GroupContainer(mem)
+Set::Set(MemoryManager &mem) : AbstractSet(mem)
 {
-    this->_memory = mem;
+    if(_memory <= 0)
+    {
+        throw Container::Error("Memory is non correct");
+    }
+    this->_memory = _memory;
 }
 
 Set::~Set()
 {
-
+    clear();
 }
 
-void *Set::allocMem(size_t size)
+void* Set::alloc_mem(size_t size)
 {
-    return _memory.allocMem(size);
+    return Container::_memory.allocMem(size);
 }
 
-void *Set::freeMem(void * ptr)
+void Set::free_mem(void * ptr)
 {
-    _memory.freeMem(ptr)
+    Container::_memory.freeMem(ptr);
 }
 
 int Set::insert(void* element, size_t size)
 {
-    void * new_element = allocMem(size);
-    this->_elements = 
+    if(checkMemory(size))
+    {
+        return 1;
+    }
+
+    *(this->_elements + this->_count_elements) = this->alloc_mem(size);
+    std::memcpy(&*(this->_elements + this->_count_elements), &element, sizeof(size));
+
+    this->_memory -= size;
+    this-_count_elements++;
+    return 0;
+}
+
+int Set::checkMemory(size_t size)
+{
+    if((this->_memory + size) > this-> _memory)
+    {
+        return 1;
+    }
+    if(size <= 0 || size > this->_memory)
+    {
+        return 1;
+    }
+    return 0;
 }
 
 int Set::size()
 {
-
+    return this->_memory; 
 }
 
 size_t Set::max_bytes()
 {
-
+    return 0;
 }
 
 Set::Iterator* Set::find(void* elem, size_t size)
 {
-
+    return new Iterator;
 }
 
 Set::Iterator* Set::newIterator()
 {
-
+    return new Iterator;
 }
 
 Set::Iterator* Set::begin()
 {
-
+    return new Iterator;
 }
 
 Set::Iterator* Set::end()
 {
-
+    return new Iterator;
 }
 
-void remove(Container::Iterator* iter)
+void Set::remove(Container::Iterator* iter)
+{
+    
+}
+
+void Set::clear()
 {
 
 }
 
-void clear()
+bool Set::empty()
 {
-
-}
-
-bool empty()
-{
-
+    return (this->_count_elements == 0) ? true : false;
 }
--- Set.h
+++ Set.h
@@ -2,50 +2,51 @@
 #include <iostream>
 #include "SetAbstract.h"
 
-class Set : public GroupContainer 
+class Set : public AbstractSet
 {
-private:
-    size_t _memory;
-    void* _elements;
-
-public:
-
-    //Constructor and destructor
-    Set(MemoryManager &mem);
-    ~Set();
-    
-
-    class Iterator : public AbstractSet::Iterator
-    
-    private: 
-
+    private:
+        size_t _memory;
+        void* _elements;
+        unsigned int _count_elements;
     public:
-        Iterator();
-        ~Iterator();
 
-        void* getElement(size_t& size) override;
-        bool hasNext() override;
-        void goToNext() override;
-        bool equals(Container::Iterator* right) override;
+        //Constructor and destructor
+        Set(MemoryManager &mem);
+        ~Set();
+
+        class Iterator : public AbstractSet::Iterator
+    {
+        private:
+
+        public:
+            Iterator();
+            ~Iterator();
+
+            void* getElement(size_t& size) override;
+            bool hasNext() override;
+            void goToNext() override;
+            bool equals(Container::Iterator* right) override;
     };
-    
-    //Methods AbstractSet
-    int insert(void *element, size_t size) override;
 
-    //Methods for work with memory allocated
-    void *alloc_mem(size_t size);
-    void free_mem(void *ptr);
+        //Methods AbstractSet
+        int insert(void *element, size_t size) override;
 
-    
-    //Standard Container Methods override
-    int size() override;
-    size_t max_bytes() override;
-    Set::Iterator* find(void* elem, size_t size) override;
-    Set::Iterator* newIterator() override;
-    Set::Iterator* begin() override;
-    Set::Iterator* end() override;
-    void remove(Container::Iterator *iter);
-    void clear() override;
-    bool empty() override;
+        //Methods for work with memory allocated
+        void *alloc_mem(size_t size);
+        void free_mem(void *ptr);
+
+
+        int checkMemory(size_t size);
+
+        //Standard Container Methods override
+        int size() override;
+        size_t max_bytes() override;
+        Set::Iterator* find(void* elem, size_t size) override;
+        Set::Iterator* newIterator() override;
+        Set::Iterator* begin() override;
+        Set::Iterator* end() override;
+        void remove(Container::Iterator *iter);
+        void clear() override;
+        bool empty() override;
 
 };
